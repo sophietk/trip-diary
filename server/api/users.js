59434b6e02aee2fbec51226711ab0c9857366f73
sophietk <sophie.tk@gmail.com>
2017-03-01 22:36:44 +0100

    tech: fix lint errors and setup lint watcher

--- server/api/users.js
+++ server/api/users.js
@@ -4,17 +4,16 @@ const crypto = require('crypto')
 const users = []
 
 module.exports = app => {
+  app.post('/signup', (req, res) => {
+    const hash = crypto.createHash('sha256')
+    hash.update(req.body.password)
 
-    app.post('/signup', (req, res) => {
-        const hash = crypto.createHash('sha256')
-        hash.update(req.body.password)
+    const user = {
+      email: req.email,
+      password: hash.digest('hex')
+    }
 
-        const user = {
-            email: req.email,
-            password: hash.digest('hex')
-        }
-
-        users.push(user)
-        res(user)
-    })
+    users.push(user)
+    res(user)
+  })
 }
--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.static('client'))
 
 require('./api/users')(app)
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 3000
 app.listen(port, function () {
   console.log(`Listening on port ${port}!`)
 })
