f964e621a0442a20c6eccf2d792eb372ab4785af
sophietk <sophie.tk@gmail.com>
2017-03-02 23:03:03 +0100

    tech: validate subscribe/signin requests

--- server/api/users.js
+++ server/api/users.js
@@ -1,37 +1,54 @@
 const crypto = require('crypto')
+const validate = require('express-validation')
+const Joi = require('joi')
 
 // in memory database for now
 const users = []
 
 module.exports = app => {
-  app.post('/api/signup', (req, res) => {
-    const hash = crypto.createHash('sha256')
-      .update(req.body.password)
-      .digest('hex')
+  app.post('/api/signup',
+    validate({
+      body: {
+        name: Joi.string().required(),
+        email: Joi.string().email().required(),
+        password: Joi.string().regex(/[a-zA-Z0-9]{3,30}/).required()
+      }
+    }),
+    (req, res) => {
+      const hash = crypto.createHash('sha256')
+        .update(req.body.password)
+        .digest('hex')
 
-    const user = {
-      name: req.body.name,
-      email: req.body.email,
-      hash: hash
-    }
+      const user = {
+        name: req.body.name,
+        email: req.body.email,
+        hash: hash
+      }
 
-    users.push(user)
-    res.send(user)
-  })
+      users.push(user)
+      res.send(user)
+    })
 
-  app.post('/api/signin', (req, res) => {
-    const email = req.body.email
-    const hash = crypto.createHash('sha256')
-      .update(req.body.password)
-      .digest('hex')
+  app.post('/api/signin',
+    validate({
+      body: {
+        email: Joi.string().email().required(),
+        password: Joi.string().required()
+      }
+    }),
+    (req, res) => {
+      const email = req.body.email
+      const hash = crypto.createHash('sha256')
+        .update(req.body.password)
+        .digest('hex')
 
-    const signinUser = users.find(user => user.email === email && user.hash === hash)
+      const signinUser = users.find(user => user.email === email && user.hash === hash)
 
-    if (!signinUser) {
-      res.sendStatus(404)
-      return
-    }
+      if (!signinUser) {
+        res.sendStatus(404)
+        return
+      }
 
-    res.send(signinUser)
-  })
+      res.send(signinUser)
+    })
 }
